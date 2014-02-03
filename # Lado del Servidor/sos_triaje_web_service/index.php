666eb4e043d569bd6f5d229bca10c339c7311e4f
Tony Briceño <tonybp18@gmail.com>
2014-02-03 01:11:43 -0430

    Actualización de las consultas SQL a PDO

--- # Lado del Servidor/sos_triaje_web_service/index.php
+++ # Lado del Servidor/sos_triaje_web_service/index.php
@@ -1,17 +1,55 @@
-
 <?php
 
 /**
  * WEB SERVICE PARA LA BD SOS_TRIAJE
  */
 
-    # Muestra que drivers de BD soporta (Se agregan en las extensiones de php)
-    //var_dump(PDO::getAvailableDrivers()); 
+# Muestra que drivers de BD soporta (Se agregan en las extensiones de php)
+//var_dump(PDO::getAvailableDrivers()); 
 
-    # Se establece la conexión a la BD y se incorpora las funcionalidades que usará la aplicación
-    require_once __DIR__ . '\protegido\db_functions.php';
-    $var = new DB_FUNCTIONS();
+if (false /*VERIFICA QUE EXISTAN LOS CAMPOS POST*/) { 
+	echo "<h1>Forbidden</h1><p>You don't have permission to access " . __DIR__ . " on this server.</p>";
+}else{
 
-    $var->get_all_actor_sistema();
+    # Se establece la conexión a la BD y se incorpora las funcionalidades que usará la aplicación.
+	require_once __DIR__ . '\protegido\db_functions.php';
+	$var = new DB_FUNCTIONS();
 
+	# Arreglo para la respuesta.
+    //$respuesta = array("tag" => $tag, "success" => 0, "error" => 0);
+
+	$var->get_all_actor_sistema();
+
+	/*
+	$response["success"] = 1;
+	$response["uid"] = "UUID";
+	$response["user"]["name"] = "name";
+	$response["user"]["email"] = "email";
+
+	echo json_encode($response);
+	/**/
+
+/*
+	# CREATE_READ_UPDATE_DELETE.
+	switch ('default') {
+		
+		case 'login':
+			# code...
+			break;
+
+		case 'READ_historial_caso':
+			# code...
+			break;
+
+		case 'caso':
+			# code...
+			break;
+
+		default:
+			# code...
+			echo "<h1>Invalid Request</h1><p>Your broser sent an invalid request.</p>";
+			break;
+	}
+	/**/
+}
 ?>
--- # Lado del Servidor/sos_triaje_web_service/protegido/db_functions.php
+++ # Lado del Servidor/sos_triaje_web_service/protegido/db_functions.php
@@ -15,29 +15,29 @@ class DB_FUNCTIONS {
         try {
             # *** Conexión fallida ***
             #$this->db = new PDO("mysql:host=localhost;dbname=database", DB_USER , DB_PASSWORD );
-            
+
             # Modo Silencioso:
             //$this->db = new PDO("mysql:host=" . DB_SERVER .";dbname=" . DB_DATABASE , DB_USER , DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT));
-            
+
             # Modo de Advertencia:
             $this->db = new PDO("mysql:host=" . DB_SERVER .";dbname=" . DB_DATABASE , DB_USER , DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
             
             # Modo de Excepción:
             //$this->db = new PDO("mysql:host=" . DB_SERVER .";dbname=" . DB_DATABASE , DB_USER , DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        
+
         } catch ( PDOException $e ) {
             # echo 'Conexi&oacute;n Fallida: <br>' . $e->getMessage();
             exit('Conexi&oacute;n Fallida: <br>' . $e->getMessage());
         }
 
-        echo '<br><br>se estableci&oacute; la conexi&oacute;n desde function' . '<br>';
+        #echo '<br><br>se estableci&oacute; la conexi&oacute;n desde function' . '<br>';
         /**/
     }
     
     # Destructor
     function __destruct() {
         $this->db = NULL;
-        echo '<br><br>DESTRUCTOR DESDE FUNCTION' . '<br>';
+        #echo '<br><br>DESTRUCTOR DESDE FUNCTION' . '<br>';
     }
 
     /**
@@ -49,17 +49,41 @@ class DB_FUNCTIONS {
         // PDO, prepared statement
         $params = array(':id' => 2);
 
-        $stmt = $this->db->prepare('SELECT * FROM actor_sistema WHERE id = :id');
-        #$stmt = PDO::prepare('SELECT * FROM actor_sistema WHERE id = :id');
+        #$stmt = $this->db->prepare('SELECT * FROM actor_sistema WHERE id = :id');
+        
+        $query = 
+        'SELECT *
+        FROM actor_sistema
+        WHERE id = :id';
+
+        $query = 
+        'SELECT *
+        FROM actor_sistema';
+
+        $stmt = $this->db->prepare($query);
 
         $stmt->execute($params);
 
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        /*
+        while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
             echo $row['mail'] . '<br>';
-        }  
+        }
         /**/
 
- }
+        $array = $stmt->fetchAll( PDO::FETCH_ASSOC );
+        $json = json_encode( $array );
+        echo $json;
+        /**/
+
+    }
+
+/*
+    public function get_historial_caso(){}
+
+    public function get_historial_caso_by_state(){}
+
+    public function get_historial_caso_by_state(){}
+/**/
 
 }
 ?>
