74e63255c91113042f54c952d256302c62a302ac
Tony Briceño <tonybp18@gmail.com>
2014-03-20 16:25:52 -0430

    ACTIVANDO PERIMSOS PARA ACEDER A LA BD DE ELASTIX

--- # Lado del Servidor/sos_triaje_web_service/protegido/db_config.php
+++ # Lado del Servidor/sos_triaje_web_service/protegido/db_config.php
@@ -16,4 +16,31 @@ define('DB_USER', "root"); 				# Usuario de la BD
 define('DB_PASSWORD', "Tajrh123654"); 	# Contraseña de la BD
 /**/
 
+/*
+# Control de acceso - https://dev.mysql.com/doc/refman/5.0/es/connection-access.html #
+# Conectarse a MySQL de Elastix desde otra red - desde WAMP para consultar y crear los webservices #
+Fuente: http://www.phpro.org/articles/Database-Connection-Failed-Mysqlnd-Cannot-Connect-To-MySQL.html
+
+# Revisar la longitud del hash de los usuarios
+SELECT user, Length(Password) FROM mysql.user;
+
+# Quitar la bandera de contraseñas viejas para que no las siga usando
+SET SESSION old_passwords=0;
+
+# Actualizar la contraseña
+UPDATE mysql.user SET Password = PASSWORD('Tajrh123654') WHERE user = 'root';
+
+# "... decir al servidor que vuelva a leer las tablas de permisos. De otro modo, los cambios no se tienen en cuenta hasta que se reinicie el servidor."
+# fuente: https://dev.mysql.com/doc/refman/5.0/es/adding-users.html
+FLUSH PRIVILEGES;
+
+# "... cuentas de superusuario con plenos permisos para hacer cualquier cosa."
+# "... Una cuenta ('usuario'@'%') puede usarse para conectarse desde cualquier otro equipo."
+# fuente: https://dev.mysql.com/doc/refman/5.0/es/adding-users.html
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+# Listar los permisos
+SHOW GRANTS;
+
+/**/
 ?>
