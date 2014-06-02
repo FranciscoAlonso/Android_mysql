8eeb555b03d7467a8cb73897df73e4feae231b0a
Tony Briceño <tonybp18@gmail.com>
2014-06-02 18:47:33 -0430

    Creacion de estructura de acceso al WS (principal) -caso getEspecialidades

--- # Lado del Servidor/sos_triaje_web_service/index.php
+++ # Lado del Servidor/sos_triaje_web_service/index.php
@@ -3,14 +3,28 @@
 * Acceso principal al WEB SERVICE (WS)...
 * Definición de cada una de las funcionalidades del WS.
 */
-echo 'Acceso principal al WEB SERVICE (WS)' . '<br>';
+echo 'Acceso principal al WEB SERVICE (WS)...' . '<br>';
 
 # Autenticación???
 
 # Obtener los parametros GET
+# Investigar Injection a este nivel
+$WS_request_type = $_GET['request'];
 
-# Realizar un switch para determinar que accion desea solicitar
+echo 'La solicitud fue de tipo: ' . $WS_request_type . '<br>';
 
+# Realizar un switch para determinar que acción desea solicitar
+switch ( $WS_request_type ) {
 # Invocar al método correspondiente en Controllers
+	case 'getEspecialidades':
+		# code...
+			
+		break;
+	
+	default:
+		# code...
+		break;
+
+}
 
 ?>
